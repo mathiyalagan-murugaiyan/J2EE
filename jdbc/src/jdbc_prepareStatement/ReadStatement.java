d4aca8042d51db556df59ba43b92e016ce5c89ae
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-10-26 12:10:16 +0530

    readStatements

--- jdbc/src/jdbc_prepareStatement/ReadStatement.java
+++ jdbc/src/jdbc_prepareStatement/ReadStatement.java
@@ -2,7 +2,6 @@
 package jdbc_prepareStatement;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,25 +17,26 @@ public class ReadStatement {
 //		
 		Connection con = ConnectionClass.getconnection();
 		
-		Scanner in = new Scanner(System.in);
+		try (Scanner in = new Scanner(System.in)) {
+			System.out.println("Enter the id");
+			
+			int userid = in.nextInt();
+			
+			PreparedStatement pst = con.prepareStatement("select * from student where id = ?");
+			
+			pst.setInt(1, userid);
+			
+			ResultSet rs = pst.executeQuery();
+			
+			rs.next();
+			int id=rs.getInt(1);
+			String name =rs.getString(2);
+			String email =rs.getString(3);
+			Long mobile =rs.getLong(4);
+			
+			System.out.println(id +" | " + name+ " | " +email+ " | "+ mobile);
+		}
 		
-		System.out.println("Enter the id");
-		
-		int userid = in.nextInt();
-		
-		PreparedStatement pst = con.prepareStatement("select * from student where id = ?");
-		
-		pst.setInt(1, userid);
-		
-		ResultSet rs = pst.executeQuery();
-		
-		rs.next();
-		int id=rs.getInt(1);
-		String name =rs.getString(2);
-		String email =rs.getString(3);
-		Long mobile =rs.getLong(4);
-		
-		System.out.println(id +" | " + name+ " | " +email+ " | "+ mobile);
 		con.close();
 		
 
