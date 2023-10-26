3751361c79377bfe9675c45522db0389f45a83d0
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-10-26 12:08:50 +0530

    connection class

--- jdbc/src/connection/ConnectionClass.java
+++ jdbc/src/connection/ConnectionClass.java
@@ -4,14 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConnectionClass {
-	
-	
-	public static Connection getconnection() throws SQLException {
-		
-		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdata", "root", "root");
+public interface ConnectionClass {
+
+	static final String url = "jdbc:mysql://localhost:3306/studentdata";
+
+	static final String username = "root";
+   
+	static final String password = "root";
+
+	static Connection getconnection() throws SQLException {
+
+		Connection con = DriverManager.getConnection(url, username, password);
 		return con;
 	}
-	
 
 }
