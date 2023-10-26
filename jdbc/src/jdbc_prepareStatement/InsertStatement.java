8c58734b88cfd8e5baccb187e6321fbff6b06f90
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-10-26 11:09:16 +0530

    prepareStatements

---
+++ jdbc/src/jdbc_prepareStatement/InsertStatement.java
@@ -0,0 +1,42 @@
+package jdbc_prepareStatement;
+
+import java.util.*;
+import java.sql.*;
+
+public class InsertStatement {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdata", "root", "root");
+
+		try (Scanner in = new Scanner(System.in)) {
+			System.out.println("Enter the id");
+
+			int id = in.nextInt();
+
+			System.out.println("Enter the name");
+
+			String name = in.next();
+
+			System.out.println("Enter the E-mail");
+
+			String email = in.next();
+
+			System.out.println("Enter the mobile number");
+
+			long mobile = in.nextLong();
+
+			PreparedStatement pst = con.prepareStatement("insert into student values (?,?,?,?)");
+
+			pst.setInt(1, id);
+			pst.setString(3, email);
+			pst.setString(2, name);
+			pst.setLong(4, mobile);
+			System.out.println(pst.executeUpdate());
+		}
+
+		con.close();
+
+	}
+
+}
