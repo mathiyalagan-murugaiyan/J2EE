1e27cf8cd1a3c47d84625eb68388c6a18d732d08
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-10-27 10:16:59 +0530

    jdbc single connection argument

---
+++ jdbc/src/jdbc_statement/JDBC_Single_Connection_Arguments.java
@@ -0,0 +1,25 @@
+package jdbc_statement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBC_Single_Connection_Arguments {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql:///studentdata?user=root&password=root");
+		
+		Statement st = con.createStatement();
+		
+		String query = "delete from student where id =4";
+		
+		int result = st.executeUpdate(query);
+		
+		System.out.println(result);
+		
+		con.close();
+	}
+
+}
