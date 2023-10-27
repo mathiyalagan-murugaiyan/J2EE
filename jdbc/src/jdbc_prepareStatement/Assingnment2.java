50dd6e9de9c89cb9cdc835b2163dbca44d78cdef
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-10-27 12:42:15 +0530

    assingnment2

---
+++ jdbc/src/jdbc_prepareStatement/Assingnment2.java
@@ -0,0 +1,39 @@
+package jdbc_prepareStatement;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class Assingnment2 {
+
+	public static void main(String[] args) throws SQLException, IOException {
+		
+		FileInputStream fis = new FileInputStream("E:\\J2EE\\jdbc\\src\\abc.properties");
+		
+		Properties p = new Properties();
+		
+		p.load(fis);
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdata",p );
+		
+		Statement st = con.createStatement();
+		
+		String query = "delete from student where id= 8";
+		
+		 int result = st.executeUpdate(query);
+		 
+		 System.out.println( "one row dleted" + result);
+		 
+		 con.close();
+		
+		
+		
+		
+		
+
+	}
+
+}
