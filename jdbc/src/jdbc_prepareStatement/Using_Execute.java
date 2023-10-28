24609fcc9b7bc4a389c0025baf1a642c5123310c
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-10-28 16:08:36 +0530

    using execute

---
+++ jdbc/src/jdbc_prepareStatement/Using_Execute.java
@@ -0,0 +1,35 @@
+package jdbc_prepareStatement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Using_Execute {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdata", "root","root");
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Enter the id");
+		
+		int id = in.nextInt();
+
+		PreparedStatement pst = con.prepareStatement("delete from student where id =?");
+		
+		pst.setInt(1, id);
+		
+		boolean result = pst.execute();
+		
+		System.out.println(result);
+		
+		System.out.println(pst.getUpdateCount());
+	}
+
+}
