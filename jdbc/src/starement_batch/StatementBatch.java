2b5a25bf7aff267a243151971e57f2731b92f540
“mathiyalagan-murugaiyangit config --global user.email “mathiyalaga930@gmail.com <mathiyalagan930@gmail.com>
2023-11-02 11:14:52 +0530

    statement batch

---
+++ jdbc/src/starement_batch/StatementBatch.java
@@ -0,0 +1,30 @@
+package starement_batch;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+
+public class StatementBatch {
+
+	public static void main(String[] args) throws SQLException {
+		
+		Connection con =DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdata?user=root&password=root");
+		
+		Statement st = con.createStatement();
+		
+		st.addBatch("delete from student where id>10");
+		
+		
+		st.addBatch("update student set mobile=1234567891 where id = 15");
+		
+      	int[] arr = st.executeBatch();
+
+	    Arrays.stream(arr)
+		.forEach(System.out::println);
+		
+       con.close();
+	}
+
+}
